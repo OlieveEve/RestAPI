d4e9a02c6682e0b4032d43b047656c06ab4d7c95
Wanida Khamprapai <wanida.kum@ku.th>
2025-02-26 20:43:46 +0700

    Update teamScreen.dart

--- lib/teamAPI/teamScreen.dart
+++ lib/teamAPI/teamScreen.dart
@@ -2,35 +2,46 @@ import 'package:flutter/material.dart';
 import './model.dart';
 import './apiService.dart';
 
-class teamData extends StatelessWidget {
+class teamData extends StatefulWidget {
   teamData({super.key});
 
+  @override
+  State<teamData> createState() => _teamDataState();
+}
+
+class _teamDataState extends State<teamData> {
   teamAPI api = teamAPI();
 
   @override
   Widget build(BuildContext context) {
-    api.getTeams();
+    // api.getDogs();
     return Scaffold(
-        // appBar: AppBar(
-        //   backgroundColor: Colors.blue,
-        // ),
-        // body: FutureBuilder(
-        //     future: api.getTeams(),
-        //     builder: (context, snapshot) {
-        //       if (snapshot.hasData) {
-        //         return ListView.builder(
-        //           itemBuilder: (context, index) {
-        //             Teams teams = snapshot.data[index];
-        //             return ListTile(
-        //               title: Text(teams.abbreviation),
-        //               subtitle: Text(teams.city),
-        //             );
-        //           },
-        //         );
-        //       } else {
-        //         return CircularProgressIndicator();
-        //       }
-        //     }),
-        );
+      appBar: AppBar(backgroundColor: Colors.amber),
+      body: FutureBuilder(
+          future: api.getDogs(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Dogs dogs = snapshot.data; //ถ้าข้อมูลที่ได้จาก API มีข้อมูลเดียว
+              return Column(
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    'Random dog image',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                  ),
+                  SizedBox(height: 20),
+                  Image(image: NetworkImage(dogs.message)),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: Text('Random'))
+                ],
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          }),
+    );
   }
 }
