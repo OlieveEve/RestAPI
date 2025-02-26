0e4628bbc6bae1c820564bd0dfad5cc4b5689b42
Wanida Khamprapai <wanida.kum@ku.th>
2025-02-26 20:43:39 +0700

    Update apiService.dart

--- lib/teamAPI/apiService.dart
+++ lib/teamAPI/apiService.dart
@@ -3,24 +3,41 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import './model.dart';
 
+// class teamAPI {
+//   Future getTeams() async {
+//     List teamList = [];
+//     var response = await http.get(Uri.https('balldontlie.io', 'api/v1/teams'));
+//     if (response.statusCode == 200) {
+//       print(response.body);
+//       var jsonData = jsonDecode(response.body);
+//       for (var data in jsonData['data']) {
+//         final team = Teams(
+//           abbreviation: data['abbreviation'],
+//           city: data['city'],
+//         );
+//         teamList.add(team);
+//       }
+//       // print(teamList.length);
+//     } else {
+//       throw Exception('Failed to load data');
+//     }
+//     return teamList;
+//   }
+// }
+
 class teamAPI {
-  Future getTeams() async {
-    List teamList = [];
-    var response = await http.get(Uri.https('balldontlie.io', 'api/v1/teams'));
+  Future getDogs() async {
+    Dogs randomDog; //ถ้าข้อมูลที่ได้จาก API มีข้อมูลเดียว
+    var response =
+        await http.get(Uri.https('dog.ceo', 'api/breeds/image/random'));
     if (response.statusCode == 200) {
-      print(response.body);
       var jsonData = jsonDecode(response.body);
-      for (var data in jsonData['data']) {
-        final team = Teams(
-          abbreviation: data['abbreviation'],
-          city: data['city'],
-        );
-        teamList.add(team);
-      }
-      // print(teamList.length);
+      randomDog =
+          Dogs.fromJson(jsonData); //ถ้าข้อมูลที่ได้จาก API มีข้อมูลเดียว
     } else {
       throw Exception('Failed to load data');
     }
-    return teamList;
+
+    return randomDog;
   }
 }
