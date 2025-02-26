78fc982490387b441e6876414b8edfe4efbe73d5
Wanida Khamprapai <wanida.kum@ku.th>
2025-02-26 20:43:42 +0700

    Update model.dart

--- lib/teamAPI/model.dart
+++ lib/teamAPI/model.dart
@@ -1,35 +1,35 @@
-class Teams {
-  String abbreviation;
-  String city;
+// class Teams {
+//   String abbreviation;
+//   String city;
 
-  Teams({required this.abbreviation, required this.city});
+//   Teams({required this.abbreviation, required this.city});
 
-  Teams.fromJson(Map<String, dynamic> json)
-      : abbreviation = json['abbreviation'],
-        city = json['city'];
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['abbreviation'] = this.abbreviation;
-    data['city'] = this.city;
-    return data;
-  }
-}
-
-// class Dogs {
-//   String message;
-//   String status;
-
-//   Dogs({required this.message, required this.status});
-
-//   Dogs.fromJson(Map<String, dynamic> json)
-//       : message = json['message'],
-//         status = json['status'];
+//   Teams.fromJson(Map<String, dynamic> json)
+//       : abbreviation = json['abbreviation'],
+//         city = json['city'];
 
 //   Map<String, dynamic> toJson() {
 //     final Map<String, dynamic> data = new Map<String, dynamic>();
-//     data['message'] = this.message;
-//     data['status'] = this.status;
+//     data['abbreviation'] = this.abbreviation;
+//     data['city'] = this.city;
 //     return data;
 //   }
 // }
+
+class Dogs {
+  String message;
+  String status;
+
+  Dogs({required this.message, required this.status});
+
+  Dogs.fromJson(Map<String, dynamic> json)
+      : message = json['message'],
+        status = json['status'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['status'] = this.status;
+    return data;
+  }
+}
